927a66cc7183ad7a8b33ab79f08a1cd6c31e4b83
Troy Bohonko <43484842+Tbohonko@users.noreply.github.com>
2019-10-28 16:47:36 -0400

    We can move

---
+++ Assets/Scenes/CameraMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    private Transform playerLocation;
+    private Vector3 CameraOffset;
+    public bool CamLock;
+    //public float MinX;
+    //public float MaxX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerLocation = GameObject.FindGameObjectWithTag("Player").transform;
+        CameraOffset = transform.position - playerLocation.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!CamLock)
+        {
+            Vector3 newLocation = transform.position;
+            newLocation.x = playerLocation.position.x + CameraOffset.x;
+
+            transform.position = newLocation;
+        }
+    }
+}
--- Assets/Scenes/PlayerControl.cs
+++ Assets/Scenes/PlayerControl.cs
@@ -1,20 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerControl : MonoBehaviour
 {
+    public float PlayerMoveSpeed;
+    public float JumpHeight;
+    private Rigidbody playerRig;
+    //public float PlayerRunSpeed;
+    //private bool isRunning;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRig = GetComponent<Rigidbody>();
+    }
+
+    bool GroundCheck()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 0.5f))
+        {
+            if (hit.transform.CompareTag("Ground"))
+                return true;
+        }
+
+        return false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float HorizontalMovement = Input.GetAxisRaw("Horizontal");
+        float VerticalMovement = Input.GetAxisRaw("Vertical");
+
+        //Add Jump stuff
+        if(GroundCheck() && Input.GetButtonDown("Jump"))
+        {
+            //playerRig.AddForce(transform.up * JumpHeight * Time.deltaTime);
+            playerRig.AddForce(0, 1 * JumpHeight, 0, ForceMode.Impulse);
+            //transform.position += transform.up * Time.deltaTime * JumpHeight;
+        }
+
+        transform.position += transform.right * Time.deltaTime * PlayerMoveSpeed * HorizontalMovement;
+        transform.position += transform.forward * Time.deltaTime * PlayerMoveSpeed * VerticalMovement;
     }
 }
