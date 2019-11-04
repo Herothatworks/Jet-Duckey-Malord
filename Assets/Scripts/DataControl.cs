2010c8d25677378be25e078e219f23b772513f0b
George "Neohanja" Hall <canunja@gmail.com>
2019-11-04 08:32:02 -0500

    Moved some things, organized folders, made player boundaries.

---
+++ Assets/Scripts/DataControl.cs
@@ -0,0 +1,10 @@
+﻿//Do not modify these, they are just structures I will be using to make life a little easier.
+//thank you for your time, and feel free to ask any questions you may have.
+using System;
+
+[Serializable]
+public class MoveLimits
+{
+    public float Upper;
+    public float Lower;
+}
